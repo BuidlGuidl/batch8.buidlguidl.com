c9de764263ac5620dcd7115341e7d80c21e1cf38
Derrek <80121818+derrekcoleman@users.noreply.github.com>
2024-07-31 14:58:08 -0600

    Update repo for Batch 8

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -14,7 +14,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const externalContracts = {
   10: {
     BatchRegistry: {
-      address: "0x0A7d97d392e7400D15460ae0C9799951a3719393",
+      address: "0x429145ce9A95D7C78D9A9EF2787842983eA4A403",
       abi: [
         {
           inputs: [
