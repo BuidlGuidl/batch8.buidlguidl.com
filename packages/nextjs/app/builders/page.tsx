13a90c4c9aa617184603e4f3499328274a2b41c4
Rohan Ahire <75326769+rohan-ahire06@users.noreply.github.com>
2024-08-14 11:48:34 +0530

    Builders page UI completed
    - Displayed the static total builder
       count.
    - Created a section for listing builder
       addresses.
    - Implemented links that direct users
       to a specific builder's profile upon
       clicking the address.

--- packages/nextjs/app/builders/page.tsx
+++ packages/nextjs/app/builders/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Link from "next/link";
+import { Address } from "~~/components/scaffold-eth";
 
 const BuildersPage = () => {
-  return <div>BuildersPage</div>;
+  const address = "0x4a9A95B6fe3b9416f0c78A8735Aa075c75AF46a4";
+  return (
+    <div className=" flex flex-col justify-center items-center md:my-20">
+      <p className=" text-[22px] font-medium underline">🏰 The Builders 🏗️ of BuidlGuidl: Batch #8 </p>
+      <div className=" flex items-center gap-x-2 md:mb-8 mb-5 ">
+        <p className=" text-lg font-normal text-[25px]">Builder : </p>
+        <p className=" text-lg font-bold text-[24px]">10</p>
+      </div>
+
+      <div className=" flex justify-center flex-row flex-wrap gap-x-5 md:mx-[200px]">
+        <Link href={`/builders/${address}`}>
+          <div className="bg-primary-content text-secondary px-3 py-2 rounded-[10px] my-3">
+            <Address address={address} />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default BuildersPage;
