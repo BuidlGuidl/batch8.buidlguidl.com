0acaedc8890072002267c6d05b895a64908bdb0b
Indra Lukmana <indralukmana@live.com>
2024-08-15 20:06:55 +0700

    refactor: :recycle: change the builder page type to NextPage

--- packages/nextjs/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d6969357C463C3/page.tsx
+++ packages/nextjs/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d6969357C463C3/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import type { NextPage } from "next";
 import IndraAboutMeSection from "~~/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d6969357C463C3/_components/IndraAboutMeSection";
 import IndraSkillsSection from "~~/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d6969357C463C3/_components/IndraSkillsSection";
 import IndraSocialsSection from "~~/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d6969357C463C3/_components/IndraSocialsSection";
@@ -6,7 +6,7 @@ import IndraSparkleName from "~~/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d69693
 import { indraEthAddress } from "~~/app/builders/0x80Ad2861Ab5D4EeA61330A4bd7d6969357C463C3/_data/indra-socials";
 import { Address } from "~~/components/scaffold-eth";
 
-const BuilderPage: React.FC = () => {
+const BuilderPage: NextPage = () => {
   return (
     <main className="container mx-auto px-4 py-8 space-y-8">
       <div className="flex flex-col md:flex-row items-center justify-between">
