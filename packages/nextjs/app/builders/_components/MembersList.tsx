faae7633649288473f2b1a81325cd8fb184ef988
Rohan Ahire <75326769+rohan-ahire06@users.noreply.github.com>
2024-08-16 04:34:54 +0530

    added a loading spinner in MembersList component

--- packages/nextjs/app/builders/_components/MembersList.tsx
+++ packages/nextjs/app/builders/_components/MembersList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import MemberAddress from "./MemberAddress";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useScaffoldEventHistory } from "~~/hooks/scaffold-eth";
 
 const MembersList = () => {
@@ -15,6 +16,11 @@ const MembersList = () => {
 
   return (
     <div className=" flex justify-center flex-row flex-wrap gap-x-5 md:mx-[200px]">
+      {!MembersList && (
+        <div className=" animate-spin text-lg">
+          <AiOutlineLoading3Quarters />
+        </div>
+      )}
       {MembersList?.map((member, i) => {
         return <MemberAddress key={i} address={member.args.builder} />;
       })}
