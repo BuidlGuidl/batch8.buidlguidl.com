7ace2e3c138ff87ff248dbdb4e0ab8d8a274d59f
benhur <benhurbenny@iclould.com>
2024-08-21 20:45:26 +0530

    some minor changes regarding the alert

--- packages/nextjs/components/CheckInFlag.tsx
+++ packages/nextjs/components/CheckInFlag.tsx
@@ -9,7 +9,7 @@ export const CheckInFlag = () => {
       className={`${isCheckedIn ? "bg-green-600" : "bg-red-500 mr-3"} ${isConnected ? "" : "hidden"} px-3 py-2 sm:py-1.5 rounded-full shadow-xl font-semibold text-sm `}
       onClick={() =>
         isCheckedIn
-          ? alert(`You have checkedIn using the above contract ${isCheckedIn}`)
+          ? alert(`You have checkedIn using the contract shown below ${isCheckedIn}`)
           : window.open("https://github.com/BuidlGuidl/batch8.buidlguidl.com/issues/10", "_blank")
       }
     >
