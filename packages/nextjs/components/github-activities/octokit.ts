472d13c81cd560676cc45519a2fc2ec183e63179
Indra Lukmana <indralukmana@live.com>
2024-08-27 13:59:01 +0700

    fix: :green_heart: change expired github access token

--- packages/nextjs/components/github-activities/octokit.ts
+++ packages/nextjs/components/github-activities/octokit.ts
@@ -1,7 +1,5 @@
 import { Octokit } from "octokit";
 
-const token =
-  process.env.NEXT_PUBLIC_GITHUB_TOKEN ||
-  "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";
+const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN || "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";
 
 export const octokit = new Octokit({ auth: token });
