08a58eedb03eba0e0e117696cf92c156b0edaf0e
Selpak99 <159260969+Selpak99@users.noreply.github.com>
2024-08-27 13:51:58 +0300

    Update page.tsx

--- packages/nextjs/app/builders/0x357bc010520575Ea5720aC3F7cb48fcedfc711Ed/page.tsx
+++ packages/nextjs/app/builders/0x357bc010520575Ea5720aC3F7cb48fcedfc711Ed/page.tsx
@@ -1,33 +1,31 @@
-import { BriefcaseIcon, CodeBracketIcon } from '@heroicons/react/20/solid'
-import { NextPage } from 'next'
-import React from 'react'
-
-
+"use client"
+import { BriefcaseIcon, CodeBracketIcon } from '@heroicons/react/20/solid';
+import { NextPage } from 'next';
+import React from 'react';
 
 const handleClick1 = () => {
-    window.open('https://github.com/Selpak99', '_blank');
+    window.open('https://github.com/Selpak99', '_blank', 'noopener,noreferrer');
 };
 const handleClick2 = () => {
-    window.open('https://app.buidlguidl.com/builders/0x357bc010520575Ea5720aC3F7cb48fcedfc711Ed', '_blank');
+    window.open('https://app.buidlguidl.com/builders/0x357bc010520575Ea5720aC3F7cb48fcedfc711Ed', '_blank', 'noopener,noreferrer');
 };
 
-
-const page: NextPage = () => {
+const Page: NextPage = () => {
     return (
         <>
             <div className="rating gap-1 mx-auto mt-10">
                 <input type="radio" name="rating-3" className="mask mask-heart bg-red-400" />
-                <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400"  />
+                <input type="radio" name="rating-3" className="mask mask-heart bg-orange-400" />
                 <input type="radio" name="rating-3" className="mask mask-heart bg-yellow-400" />
                 <input type="radio" name="rating-3" className="mask mask-heart bg-lime-400" />
-                <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" defaultChecked/>
+                <input type="radio" name="rating-3" className="mask mask-heart bg-green-400" defaultChecked />
             </div>
             <div className="flex items-center flex-col flex-grow pt-10">
                 <div className="px-5">
                     <h1 className="text-center">
-                        <span className="block text-4xl font-bold">Hello,This is Selpak!</span>
+                        <span className="block text-4xl font-bold">Hello, This is Selpak!</span>
                     </h1>
-                    <p className="text-center text-lg mt-4">Blockchain Enthusiast </p>
+                    <p className="text-center text-lg mt-4">Blockchain Enthusiast</p>
                 </div>
 
                 <div className="px-5 py-10 bg-base-300 rounded-lg">
@@ -41,28 +39,19 @@ const page: NextPage = () => {
 
                 <div className="flex-grow w-full mt-16">
                     <div className="flex justify-center items-center gap-8 flex-col sm:flex-row">
-                        <div className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl">
-                            <CodeBracketIcon
-                                className="h-8 w-8 fill-secondary"
-                                onClick={handleClick1}
-                            />
+                        <button className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl" onClick={handleClick1}>
+                            <CodeBracketIcon className="h-8 w-8 fill-secondary" />
                             Github
-                        </div>
-                        <div className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl">
-                            <BriefcaseIcon className="h-8 w-8 fill-secondary"
-                                onClick={handleClick2}
-                            />
+                        </button>
+                        <button className="flex flex-col bg-base-100 px-6 py-6 text-center items-center max-w-xs rounded-3xl" onClick={handleClick2}>
+                            <BriefcaseIcon className="h-8 w-8 fill-secondary" />
                             Buidl Guidl
-                        </div>
-
+                        </button>
                     </div>
                 </div>
-
-
-
             </div>
         </>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
