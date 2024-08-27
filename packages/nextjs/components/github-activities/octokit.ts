2f4ff3b6b8ebc65614e09068259a74905a64079c
Indra Lukmana <indralukmana@live.com>
2024-08-27 13:51:32 +0700

    fix: :green_heart: change old github access token

--- packages/nextjs/components/github-activities/octokit.ts
+++ packages/nextjs/components/github-activities/octokit.ts
@@ -2,6 +2,6 @@ import { Octokit } from "octokit";
 
 const token =
   process.env.NEXT_PUBLIC_GITHUB_TOKEN ||
-  "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";
+  "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";
 
 export const octokit = new Octokit({ auth: token });
