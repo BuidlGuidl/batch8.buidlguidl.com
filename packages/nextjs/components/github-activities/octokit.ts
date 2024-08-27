08bf79959bed7508341c550b814ae6771161c34f
Indra Lukmana <indralukmana@live.com>
2024-08-27 13:34:17 +0700

    feat: :sparkles: add temporary github access token

--- packages/nextjs/components/github-activities/octokit.ts
+++ packages/nextjs/components/github-activities/octokit.ts
@@ -1,5 +1,7 @@
 import { Octokit } from "octokit";
 
-const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+const token =
+  process.env.NEXT_PUBLIC_GITHUB_TOKEN ||
+  "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";
 
 export const octokit = new Octokit({ auth: token });
